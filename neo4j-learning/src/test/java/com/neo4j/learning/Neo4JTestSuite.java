74736288efc220d0c3f4f5f5238bd2f5252ee563
naluem <mdt3@alumnes.udl.cat>
2017-06-21 23:06:43 +0200

    procedures for neo4j written in java and tests

---
+++ neo4j-learning/src/test/java/com/neo4j/learning/Neo4JTestSuite.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.neo4j.learning;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author naluem
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({com.neo4j.learning.Neo4JLearning.class})
+public class Neo4JTestSuite {
+}
